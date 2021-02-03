6992e7ff4451a4cf7f7f310c095fc6cc82cd4932
Raul Yau <ryau07@gmail.com>
2021-02-03 13:23:50 -0600

    first commit

---
+++ src/main/java/com/ryau/demo/ToDoListSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ryau.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryau/demo/ToDoListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryau.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
